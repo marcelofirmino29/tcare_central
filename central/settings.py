b1931845f1a4de66a845d903191335290ce54589
mateusdesena <mateuscrvg@live.com>
2024-03-06 23:54:29 -0400

    Iniciei a construção das views

--- central/settings.py
+++ central/settings.py
@@ -132,3 +132,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#importa configurações locais
+try:
+    from central.local_settings import *
+except ImportError:
+    ...
--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from home.models import TagBle
 def index(request):
+    tags = TagBle.objects.all()
+
+    context = {
+        'tags': tags,
+    }
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
