63dd3bc19acaa1642a6b52cc662218240092ba03
mateusdesena <mateuscrvg@live.com>
2024-04-13 17:26:34 -0400

    configurei o bd para deploy no render

--- central/settings.py
+++ central/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'central.wsgi.application'
 # Banco de Dados
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'base_tcare',
+        'USER': 'usuario_tcare',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
